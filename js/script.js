ef22441f3dcf55cfffe291f43004400a1e3f6bcf
serg <serg.maiier@gmail.com>
2017-12-07 00:13:44 +0200

    add slider

--- js/script.js
+++ js/script.js
@@ -1,32 +1,17 @@
-  // $('.slider-bottom').slick({
-  //     infinite: true,
-  //     arrows: false,
-  //     dots: true,
-  //     slidesToShow: 1,
-  //     slidesToScroll: 1
-  //   });
-
-  // $('.slider-top').slick({
-  //     infinite: true,
-  //     arrows: true,
-  //     dots: false,
-  //     slidesToShow: 1,
-  //     slidesToScroll: 1
-  //   });
-
+  $('.slider-bottom').slick({
+      infinite: true,
+      arrows: false,
+      dots: true,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    });
 
   $('.slider-top').slick({
-  	slidesToShow: 1,
-  	slidesToScroll: 1,
-  	arrows: false,
-  	fade: true,
-  	asNavFor: '.slider-bottom'
-  });
-  $('.slider-bottom').slick({
-  	slidesToShow: 1,
-  	slidesToScroll: 1,
-  	asNavFor: '.slider-top',
-  	dots: true,
-  	centerMode: true,
-  	focusOnSelect: true
-  });
+      infinite: true,
+      arrows: true,
+      dots: false,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    });
+
+
